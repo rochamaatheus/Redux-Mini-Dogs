4de96b9f9647b012023ac3058546cba607021a5f
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-07-19 16:54:57 -0300

    Lógica das Fotos

--- src/Components/Photos.jsx
+++ src/Components/Photos.jsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPhotos } from '../store/photos';
+import PhotosContent from './PhotosContent';
+
 const Photos = () => {
-  return <>Fotos</>;
+  const { data } = useSelector((state) => state.photos);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchPhotos(1));
+  }, [dispatch]);
+
+  return <section>{data && <PhotosContent />}</section>;
 };
 
 export default Photos;
--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,8 +1,9 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import login from './login';
 import localStorage from './middleware/localStorage';
+import login from './login';
+import photos from './photos';
 
-const reducer = combineReducers({ login });
+const reducer = combineReducers({ login, photos });
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) =>
